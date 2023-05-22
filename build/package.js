9c8a8a859aea7017956e5f27cbeb285433b75e53
caperaven <caperaven@gmail.com>
2023-05-22 14:33:44 +0200

    update for new binding engine

--- build/package.js
+++ build/package.js
@@ -39,13 +39,13 @@ async function packageFile(sourceFile, targetFile, loader, format, minified) {
     await Deno.writeTextFile(targetFile, result.code);
 }
 
-async function bundle(file, output) {
+async function bundle(file, output, minify = true) {
     const result = await esbuild.build({
         entryPoints: [file],
         bundle: true,
         outfile: output,
         format: "esm",
-        minify: true
+        minify: minify
     })
 
     console.log(result);
@@ -53,5 +53,5 @@ async function bundle(file, output) {
 
 
 await createFolderStructure();
-await bundle("./src/crs-modules.js", "./dist/crs-modules.js");
+await bundle("./src/crs-modules.js", "./dist/crs-modules.js", false);
 Deno.exit(0);
--- dist/crs-modules.js
+++ dist/crs-modules.js
@@ -1,1 +1,118 @@
-var o=class{constructor(){this.registry={},this.componentRegistry={},this.componentLocations={},this.moduleLocations={}}async dispose(){await this.clear(),this.registry=null}async clear(){let t=Object.keys(this.registry);for(let s of t)delete this.registry[s]}async add(t,s){return this._addToRegistry(this.registry,t,s)}async addComponent(t,s){return this._addToRegistry(this.componentRegistry,t,s)}async addComponentLocation(t,s){return this._addToRegistry(this.componentLocations,t,s)}async addModuleLocation(t,s){return this._addToRegistry(this.moduleLocations,t,s)}async _addToRegistry(t,s,a){t[s]==null&&(t[s]=a)}async remove(t){delete this.registry[t]}async get(t){let s=document.body.dataset.appPath||"",a=this.registry[t];if(a!=null)return typeof a!="string"?a:(a=await import(`${s}${a}`),this.registry[t]=a);let e=t.split(":"),n=this.moduleLocations[e[0]];if(n!=null)return a=await import(`${n}/${e[1]}.js`),this.registry[t]=a}async getComponent(t,s){let a=this.componentRegistry[t];if(a!=null)return typeof a!="string"?void 0:await this.loadComponentFromPath(t,a);let e=this.componentLocations[s];e!=null&&await this.loadComponentFromPath(t,`${e}/${t}/${t}.js`)}async loadComponentFromPath(t,s){let a=document.body.dataset.appPath||"";this.componentRegistry[t]=await import(`${a}${s}`)}async getDefault(t){return(await this.get(t))?.default}async getConstant(t,s){return(await this.get(t))?.[s]}async getPrototype(t,s){return(await this.get(t))?.[s||t]}async getInstanceOf(t,s,...a){let e=await this.getPrototype(t,s||t);return e?new e(...a):null}async getInstanceOfDefault(t,...s){let a=await this.getDefault(t);return a?new a(...s):null}async call(t,s,a,...e){return await(await this.get(t))?.[s||t]?.call(a,...e)}async callDefault(t,s,...a){return await(await this.getDefault(t))?.call(s,...a)}};globalThis.crs=globalThis.crs||{};globalThis.crs.modules=new o;globalThis.crs.modules.enableBinding=async i=>{for(let t of i||[])await globalThis.crs.modules.addComponent(t[0],t[1]);globalThis.crsbinding!=null&&(globalThis.crs.modules._parseElement=globalThis.crsbinding.parsers.parseElement,globalThis.crsbinding.parsers.parseElement=async(t,s,a)=>{await globalThis.crs.modules._parseElement(t,s,a),t.dataset!=null&&await globalThis.crs.modules.getComponent(t.nodeName.toLowerCase(),t.dataset?.module)})};export{o as Modules};
+// src/crs-modules.js
+var Modules = class {
+  constructor() {
+    this.registry = {};
+    this.componentRegistry = {};
+    this.componentLocations = {};
+    this.moduleLocations = {};
+  }
+  async dispose() {
+    await this.clear();
+    this.registry = null;
+  }
+  async clear() {
+    const keys = Object.keys(this.registry);
+    for (let key of keys) {
+      delete this.registry[key];
+    }
+  }
+  async add(key, file) {
+    return this._addToRegistry(this.registry, key, file);
+  }
+  async addComponent(key, file) {
+    return this._addToRegistry(this.componentRegistry, key, file);
+  }
+  async addComponentLocation(key, path) {
+    return this._addToRegistry(this.componentLocations, key, path);
+  }
+  async addModuleLocation(key, path) {
+    return this._addToRegistry(this.moduleLocations, key, path);
+  }
+  async _addToRegistry(registry, key, value) {
+    if (registry[key] == null) {
+      registry[key] = value;
+    }
+  }
+  async remove(key) {
+    delete this.registry[key];
+  }
+  async get(key) {
+    const prefix = document.body.dataset.appPath || "";
+    let result = this.registry[key];
+    if (result != null) {
+      if (typeof result !== "string")
+        return result;
+      result = await import(`${prefix}${result}`);
+      return this.registry[key] = result;
+    }
+    const parts = key.split(":");
+    const path = this.moduleLocations[parts[0]];
+    if (path == null)
+      return;
+    result = await import(`${path}/${parts[1]}.js`);
+    return this.registry[key] = result;
+  }
+  async getComponent(nodeName, location) {
+    let result = this.componentRegistry[nodeName];
+    if (result != null) {
+      if (typeof result !== "string")
+        return;
+      return await this.loadComponentFromPath(nodeName, result);
+    }
+    const path = this.componentLocations[location];
+    if (path == null)
+      return;
+    await this.loadComponentFromPath(nodeName, `${path}/${nodeName}/${nodeName}.js`);
+  }
+  async loadComponentFromPath(nodeName, path) {
+    const prefix = document.body.dataset.appPath || "";
+    this.componentRegistry[nodeName] = await import(`${prefix}${path}`);
+  }
+  async getDefault(key) {
+    return (await this.get(key))?.default;
+  }
+  async getConstant(key, name) {
+    const module = await this.get(key);
+    return module?.[name];
+  }
+  async getPrototype(key, className) {
+    const module = await this.get(key);
+    return module?.[className || key];
+  }
+  async getInstanceOf(key, className, ...args) {
+    const result = await this.getPrototype(key, className || key);
+    return result ? new result(...args) : null;
+  }
+  async getInstanceOfDefault(key, ...args) {
+    const proto = await this.getDefault(key);
+    return proto ? new proto(...args) : null;
+  }
+  async call(key, fnName, thisObj, ...args) {
+    const module = await this.get(key);
+    const fn = module?.[fnName || key];
+    return await fn?.call(thisObj, ...args);
+  }
+  async callDefault(key, thisObj, ...args) {
+    const fn = await this.getDefault(key);
+    return await fn?.call(thisObj, ...args);
+  }
+};
+globalThis.crs = globalThis.crs || {};
+globalThis.crs.modules = new Modules();
+globalThis.crs.modules.enableBinding = async (modules) => {
+  for (let module of modules || []) {
+    await globalThis.crs.modules.addComponent(module[0], module[1]);
+  }
+  if (globalThis.crs.binding != null) {
+    globalThis.crs.modules._parseElement = globalThis.crs.binding.parsers.parseElement;
+    globalThis.crs.binding.parsers.parseElement = async (element, context, options) => {
+      await globalThis.crs.modules._parseElement(element, context, options);
+      if (element.dataset != null) {
+        await globalThis.crs.modules.getComponent(element.nodeName.toLowerCase(), element.dataset?.module);
+      }
+    };
+  }
+};
+export {
+  Modules
+};
